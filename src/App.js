d2e67b73e38a3cae38932cf588cc20546bdaf60d
Bruklin92 <meetajudiya93@gmail.com>
2025-02-20 19:38:51 +0530

    h

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React i start new topic
+          Learn React i hello
         </a>
       </header>
     </div>
